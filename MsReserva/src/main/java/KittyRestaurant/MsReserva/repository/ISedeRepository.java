b0069bdf2ec0c7c3e3907763c0fcf2834b8d6937
Rigoberto Anthony Yñape Chavez <rigo_521@hotmail.com>
2023-12-19 12:10:03 -0500

    Actualizacion de la clase SedeRepository

--- MsReserva/src/main/java/KittyRestaurant/MsReserva/repository/ISedeRepository.java
+++ MsReserva/src/main/java/KittyRestaurant/MsReserva/repository/ISedeRepository.java
@@ -1,5 +1,12 @@
 package KittyRestaurant.MsReserva.repository;
 
-public class ISedeRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import KittyRestaurant.MsReserva.model.SedeModel;
+
+@Repository
+public interface ISedeRepository extends CrudRepository <SedeModel,Integer> {
+
     
 }
