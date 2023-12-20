3727d7d9104f7f0a779708495140a79be6df397c
kqrlsefni <daremonemo@gmail.com>
2023-12-20 14:33:27 -0500

    RervaMapper modificacion

--- MsReserva/src/main/java/KittyRestaurant/MsReserva/mapper/ReservaMapper.java
+++ MsReserva/src/main/java/KittyRestaurant/MsReserva/mapper/ReservaMapper.java
@@ -1,7 +1,6 @@
 package KittyRestaurant.MsReserva.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import KittyRestaurant.MsReserva.dto.ReservaDTO;
@@ -12,26 +11,6 @@ import KittyRestaurant.MsReserva.model.ReservaModel;
 public interface ReservaMapper {
     ReservaMapper INSTANCE = Mappers.getMapper( ReservaMapper.class );
 
-    @Mapping(source = "idReserva", target = "idReserva")
-    @Mapping(source = "cantidadPersona", target = "cantidadPersona")
-    @Mapping(source = "fechaHora", target = "fechaHora")
-    @Mapping(source = "descripcion", target = "descripcion")
-    @Mapping(source = "estado", target = "estado")
-    @Mapping(source = "numeroContacto", target = "numeroContacto")
-    @Mapping(source = "nombreContacto", target = "nombreContacto")
-    @Mapping(source = "usuarioCreacion", target = "usuarioCreacion")
-    @Mapping(source = "fechaCreacion", target = "fechaCreacion")
-    @Mapping(source = "usuarioUltimaModificacion", target = "usuarioUltimaModificacion")
-    @Mapping(source = "fechaUltimaModificacion", target = "fechaUltimaModificacion")
-    @Mapping(source = "idMesa", target = "idMesa")
     ReservaModel ReserservaRequestlToReservaModel(ReservaRequest req);
-
-    @Mapping(source = "idReserva", target = "idReserva")
-    @Mapping(source = "cantidadPersona", target = "cantidadPersona")
-    @Mapping(source = "fechaHora", target = "fechaHora")
-    @Mapping(source = "descripcion", target = "descripcion")
-    @Mapping(source = "estado", target = "estado")
-    @Mapping(source = "numeroContacto", target = "numeroContacto")
-    @Mapping(source = "nombreContacto", target = "nombreContacto")
     ReservaDTO ReservaModelToReservaDTO(ReservaModel reservaModel);
 }
