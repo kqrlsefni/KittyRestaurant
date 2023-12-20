55f47607d9bf8386de3cecaca688f9fce20f8c3b
Rigoberto Anthony Yñape Chavez <rigo_521@hotmail.com>
2023-12-20 15:37:59 -0500

    Implementacion de la clase SedeResponse

---
+++ MsReserva/src/main/java/KittyRestaurant/MsReserva/dto/SedeResponse.java
@@ -0,0 +1,15 @@
+package KittyRestaurant.MsReserva.dto;
+
+public class SedeResponse<T> extends ResponseFormat {
+
+    private Iterable<T> data;
+
+    public Iterable<T> getData() {
+        return data;
+    }
+
+    public void setData(Iterable<T> data) {
+        this.data = data;
+    }
+}
+
