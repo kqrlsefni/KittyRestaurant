20bbe2d89950384002e3e241794243ae84ca50d8
Hector Lopez <hectorandreylopezmontalvan@gmail.com>
2024-01-16 20:48:30 -0500

    Creacion de 2do MS
    Se procedio con la creacion del mspedido y las carpetas de su interior

---
+++ MsPedido/src/test/java/KittyRestaurant/MsPedido/MsPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package KittyRestaurant.MsPedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
